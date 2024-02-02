49c6d0d6b1cdf324b0389f70beb2905352b6bc25
GodjaVasile2 <vasile.godja88@gmail.com>
2024-02-02 20:22:47 +0200

    add comments

--- delete_db.sql
+++ delete_db.sql
@@ -6,10 +6,3 @@ BEGIN
 END;
 /
 
-BEGIN
-   FOR t IN (SELECT table_name FROM user_tables)
-   LOOP
-      EXECUTE IMMEDIATE 'DROP TABLE ' || t.table_name || ' CASCADE CONSTRAINTS';
-   END LOOP;
-END;
-/
